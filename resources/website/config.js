44615080fd7be671e9600d2df072c36f4293bf83
ikram <belhouchet.ikram90@gmail.com>
2022-11-08 16:18:49 +0000

    second part

--- resources/website/config.js
+++ resources/website/config.js
@@ -1,4 +1,3 @@
 window.COFFEE_CONFIG = {
 	API_GW_BASE_URL_STR: "https://ta24y8m5t6.execute-api.us-east-1.amazonaws.com/prod",
-	COGNITO_LOGIN_BASE_URL_STR: null
-};
+	COGNITO_LOGIN_BASE_URL_STR: "https://ib-2022-11-08.auth.us-east-1.amazoncognito.com/login?client_id=440dm2ah4dkqu0cotosobuhien&response_type=token&scope=openid+email&redirect_uri=https://d2fhp7khwte8xh.cloudfront.net/callback.html"};
